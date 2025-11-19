1dae36d372cc34752cd12b6d01974f704731595e
serranojuan-eng <serranojuan@olayista.com>
2025-11-19 17:26:07 -0500

    Add files via upload

---
+++ # multiplicacion y suma.py
@@ -0,0 +1,20 @@
+# multiplicacion y suma
+
+A=3
+B=7
+
+C=A+B
+D=A-B
+E=A/B
+F=A*B
+
+G = input("Presiona enter para ver los resultados")
+
+print("la suma de A y B es:", C)
+
+print("la resta de A y B es:", D)
+
+print("la division de A y B es:", E)
+
+print("la multiplicacion de A y B es:", F)
+
